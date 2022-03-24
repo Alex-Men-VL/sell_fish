28305d8651609ff85b6edb5ca06eb54c71472702
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-03-24 16:50:26 +0300

    Add integration with moltin

---
+++ moltin_api.py
@@ -0,0 +1,80 @@
+import pprint
+
+import requests
+from environs import Env
+
+
+def get_access_token(client_id, client_secret):
+    url = 'https://api.moltin.com/oauth/access_token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, data=payload)
+    token_description = response.json()
+    return {'token': token_description['access_token'],
+            'expires': token_description['expires']}
+
+
+def get_products(access_token):
+    url = 'https://api.moltin.com/v2/products'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_or_create_cart(access_token, cart_id):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'X-MOLTIN-CURRENCY': 'USD'
+    }
+
+    response = requests.get(url, headers=headers)
+    pprint.pprint(response.json())
+    response.raise_for_status()
+    return response.json()
+
+
+def add_cart_item(access_token, cart_id, item_id, item_quantity):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'X-MOLTIN-CURRENCY': 'USD'
+    }
+
+    cart_item = {
+        'data': {
+            'id': item_id,
+            'type': 'cart_item',
+            'quantity': item_quantity,
+        },
+    }
+    response = requests.post(url, headers=headers, json=cart_item)
+    response.raise_for_status()
+    pprint.pprint(response.json())
+    return response.json()
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    client_id = env.str('CLIENT_ID')
+    client_secret = env.str('CLIENT_SECRET')
+    access_token = get_access_token(client_id, client_secret)
+    # print(access_token['token'])
+    products = get_products(access_token['token'])
+    pprint.pprint(products)
+
+    cart = get_or_create_cart(access_token['token'], 'test_1')
+    # add_cart_item(access_token, cart['data']['id'],
+    #               products['data'][0]['id'], 1)
+
+
+if __name__ == '__main__':
+    main()
